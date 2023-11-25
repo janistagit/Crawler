5b2e3f9ed2632ada8354bf1300b5d50f480ac99c
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2023-11-24 23:03:01 -0800

    Retrieve html and error handling

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
@@ -8,4 +9,12 @@ frontier.append("https://www.cpp.edu/sci/computer-science/")
 
 def crawlerThread(frontier):
     while len(frontier) != 0:
-        pass
+        url = frontier.pop()
+        try:
+            html = urlopen(url)
+        except HTTPError as e:
+            print(e)
+        except URLError as e:
+            print("Server could not be found.")
+        else:
+            pass
