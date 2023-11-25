a4cfebf57e4ac153e510a99503c05da514203aab
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2023-11-25 00:44:00 -0800

    Add non visited links to frontier

--- crawler.py
+++ crawler.py
@@ -8,6 +8,7 @@ import re
 frontier = []
 frontier.append("https://www.cpp.edu/sci/computer-science/")
 target = "https://www.cpp.edu/sci/computer-science/faculty-and-staff/permanent-faculty.shtml"
+visited = []
 
 client = MongoClient(host="localhost", port=27017)
 db = client.crawler
@@ -15,6 +16,7 @@ pages = db.pages
 
 def crawlerThread(frontier):
     while len(frontier) != 0:
+        visited.append(frontier[0])
         url = frontier.pop()
 
         if (re.match("^https://www.cpp.edu", url) == None):
@@ -22,20 +24,31 @@ def crawlerThread(frontier):
 
         try:
             html = urlopen(url)
+            html = html.read()
         except HTTPError as e:
             print(e)
         except URLError as e:
             print("Server could not be found.")
         else:
-            data = html.read().decode(encoding="iso-8859-1")
+            data = html.decode(encoding="iso-8859-1")
             document = {
                 "url":url,
                 "html":data
             }
             pages.insert_one(document)
 
-            bs = BeautifulSoup(html.read(), 'html.parser')
+            bs = BeautifulSoup(html, 'html.parser')
             if bs.find("h1", string="Permanent Faculty"):
                 frontier.clear()
+                print("Found")
+            else:
+                print("Not found")
+                for link in bs.find_all("a", href=True):
+                    temp = link['href']
+                    if (re.match("^https://", temp) == None):
+                        temp = "https://www.cpp.edu" + temp
+
+                    if temp not in visited:
+                        frontier.append(temp)
 
 crawlerThread(frontier)
